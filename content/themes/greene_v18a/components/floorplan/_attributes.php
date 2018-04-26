33366cde0e9a72699611afa8e4334130b291ec75
Asif Ahmed <asif@stitchdesignco.com>
2018-04-26 13:44:22 -0400

    add floor plan grid data styles

--- content/themes/greene_v18a/components/floorplan/_attributes.php
+++ content/themes/greene_v18a/components/floorplan/_attributes.php
@@ -6,13 +6,13 @@
 
 <div class="attributes">
 	<div class="rest">
-		<span class="bed">1 Bedroom</span>
+		<span class="bed">1 Bed</span>
 	</div>
 	<div class="rest">
-		<span class="bath">1 Bathroom</span>
+		<span class="bath">1 Bath</span>
 	</div>
 	<div class="rest">
-		<span class="sqft">728 Square Feet</span>
+		<span class="sqft">728 Sq Ft</span>
 	</div>
 	<div class="rest">
 		<span class="price">$$$$ - $$$$</span>
