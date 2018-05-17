d76aa1e3b568f3ac14a991715e8df2aa7712ea00
Asif Ahmed <asif@stitchdesignco.com>
2018-05-17 11:48:58 -0400

    rebuild archive floorplans subnab

--- content/themes/greene_v18a/components/floorplan/_view-subnav.php
+++ content/themes/greene_v18a/components/floorplan/_view-subnav.php
@@ -8,9 +8,15 @@
 		<h2>Whichever apartment home you choose,</h2>
 		<h2>The Greene offers fresh-air, urban living in the West End.</h2>
 	</div>
-	<div class="subnav tabs">
-		<span id="view-indv">Individual Plans</span>
-		<span id="span"></span>
-		<span id="view-map">Community Map</span>
+	<div class="subnav tab">
+		<div class="tabs individual-link">
+			<span id="view-indv" class="link individual">Individual Plans</span>
+		</div>
+		<div class="spanning-element">
+			<span id="span"></span>
+		</div>
+		<div class="tabs community-link">
+			<span id="view-map" class="link individual">Community Map</span>
+		</div>
 	</div>
 </div>
